d1263e9f6210a4a4a1dc364c90a5e19a2487ed56
Ben <akhlifa@students.beaminstitute.org>
2024-03-18 11:03:36 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const chalk = require('chalk');
+
+const colleagues = ['sam', 'marla', 'wara', 'alex',];
+
+const colors = [chalk.red, chalk.green, chalk.blue, chalk.yellow];
+
+console.log("Colleagues:");
+
+colleagues.forEach(colleague => {
+  console.log(chalk.keyword('orange')(colleague));
+});
